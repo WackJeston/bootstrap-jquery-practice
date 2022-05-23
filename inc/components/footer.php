c8049571399b7150f2051a0b18bf1289b30e29dd
WackJeston <33kcaj33@gmail.com>
2022-05-23 22:40:22 +0100

    commit

--- inc/components/footer.php
+++ inc/components/footer.php
@@ -1,10 +1,16 @@
-
-
+<!-- START OF FOOTER.PHP -->
     </main>
 
     <footer>
-      <h1>footer</h1>
+      <!-- <h1>footer</h1> -->
     </footer>
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Font Awsome -->
+    <script src="https://kit.fontawesome.com/c8712841cc.js" crossorigin="anonymous"></script>
 
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php include("inc/components/header.php") ?>
 
-<h1>main</h1>
+  <!-- Landing Zone -->
+  <section class="bg-dark text-light p-5 p-xl-0 pt-xl-5 text-center text-sm-start">
+    <div class="container-lg">
+      <div class="d-sm-flex align-items-center justify-content-between">
+        <div>
+          <h1>Become a <span class="text-warning">Web Developer</span></h1>
+          <p class="lead my-4">We focus on teaching our students the fundamentals of the latest and greatest technologies to prepare them for their first dev role</p>
+          <button class="btn btn-primary btn-lg" type="button">Start The Enrollment</button>
+        </div>
+        <img src="img/showcase.svg" alt="ShowCase Image" class="img-fluid w-50 d-none d-sm-block">
+      </div>
+    </div>
+  </section>
+
+  <!-- Newsletter Sign Up -->
+  <section class="bg-primary text-light p-5">
+    <div class="container">
+      <div class="d-md-flex justify-content-between align-items-center">
+        <h3 class="mb-3 mb-md-0">Sign Up For Newsletter</h3>
+        <div class="input-group mb-3 news-input">
+          <input type="text" class="form-control" placeholder="Email">
+          <button class="btn btn-dark btn-lg" type="button">Button</button>
+        </div>
+      </div>
+    </div>
+  </section>
 
 <?php include("inc/components/footer.php") ?>
